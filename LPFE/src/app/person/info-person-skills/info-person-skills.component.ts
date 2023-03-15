f57361de0e50c745e160ce5083c4e4e7a2e1dbb9
Christian Marino <80922734+GitMarino@users.noreply.github.com>
2023-03-15 14:29:54 +0100

    dettaglio persona

--- LPFE/src/app/person/info-person-skills/info-person-skills.component.ts
+++ LPFE/src/app/person/info-person-skills/info-person-skills.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Person } from 'src/app/model/objects/person';
+import { SkillMark } from 'src/app/model/objects/skill-marks';
+import { HttpCallsService } from '../../model/service/http-calls.service';
 
 @Component({
   selector: 'app-info-person-skills',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./info-person-skills.component.scss']
 })
 export class InfoPersonSkillsComponent {
+  
+  personId?: number;
+
   iconName: string = "file-text";
+  person?: Person;
+  skillMarks?: SkillMark[];
+
+  constructor(private activatedRoute: ActivatedRoute, private httpCalls: HttpCallsService)
+  {}
+
+  ngOnInit(): void
+  { this.activatedRoute.params.subscribe(params => {
+      this.personId = params['id'];
+      if(this.personId != undefined)
+      { this.httpCalls.getPerson(this.personId!)
+          .subscribe({
+            next: (response: Person) => {
+              this.person = response as Person;
+            }
+          });
+  
+        this.httpCalls.getSkillMarks(this.personId!)
+          .subscribe({
+            next: (response: SkillMark[]) => {
+              this.skillMarks = response as SkillMark[];
+            }
+          });
+      }
+    });   
+  }
 }
