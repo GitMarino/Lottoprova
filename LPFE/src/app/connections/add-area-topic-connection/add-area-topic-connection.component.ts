4d7b1c04ccda5beed502b8515daa1c27bedcfc2f
Christian Marino <80922734+GitMarino@users.noreply.github.com>
2023-04-14 18:25:27 +0200

    first disabled button before complited

--- LPFE/src/app/connections/add-area-topic-connection/add-area-topic-connection.component.ts
+++ LPFE/src/app/connections/add-area-topic-connection/add-area-topic-connection.component.ts
@@ -1,24 +1,41 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { AfterViewChecked, AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { Button } from 'src/app/shared/objects/button';
 import { KeyValue } from 'src/app/shared/objects/key-value';
 import { HttpCallsService } from '../../shared/service/http-calls.service';
 import { PopupComponent } from 'src/app/shared/popup/popup.component';
 import { ERROR_BODY, ERROR_TITLE, SUCCESS_BODY, SUCCESS_TITLE } from 'src/app/shared/constants/constants';
+import { Topic } from 'src/app/shared/objects/topic';
+import { BehaviorSubject, Observable, OperatorFunction, debounceTime, distinctUntilChanged, map } from 'rxjs';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-add-area-topic-connection',
   templateUrl: './add-area-topic-connection.component.html',
   styleUrls: ['./add-area-topic-connection.component.css']
 })
-export class AddAreaTopicConnectionComponent implements OnInit {
+export class AddAreaTopicConnectionComponent implements OnInit, AfterViewInit {
 
   iconName: string = 'git-commit';
   buttons: Button[] = [];
 
-  selectedTopic?: number;
-  topics: KeyValue[] = [];
   selectedArea?: number;
   areas: KeyValue[] = [];
+
+  topics: KeyValue[] = [];
+  public selectedTopic?: any;
+  inputFormatter = (selected: KeyValue) => selected.value;
+  resultsFormatter = (result: KeyValue) => result.value;
+  searchTopic: OperatorFunction<string, readonly KeyValue[]> = (text$: Observable<string>) =>
+		text$.pipe(
+			debounceTime(200),
+			distinctUntilChanged(),
+			map((topicWritten) =>
+				topicWritten.length < 3 ? []
+        : this.topics!.filter((t) => t.value.toLowerCase().indexOf(topicWritten.toLowerCase()) > -1).slice(0, 5),
+			),
+		);
+
+  @ViewChild('form') form!: NgForm;
 
   @ViewChild('popup') myPopup!: PopupComponent;
 
@@ -26,9 +43,18 @@ export class AddAreaTopicConnectionComponent implements OnInit {
   { this.buttons = [
       {
         name: 'salva',
-        action: this.addAreaTopicConnection
+        action: this.addAreaTopicConnection,
+        disabled: new BehaviorSubject<boolean>(true),
       }
     ];
+  }
+
+  ngAfterViewInit(): void {
+    this.form.statusChanges?.subscribe({
+      next: status => {
+        (this.buttons[0].disabled as BehaviorSubject<boolean>).next(status!=='VALID')
+      }
+    })
   }
 
   ngOnInit(): void
@@ -48,7 +74,7 @@ export class AddAreaTopicConnectionComponent implements OnInit {
   }
 
   addAreaTopicConnection = () =>
-  { this.httpCalls.createAreaTopicConnection(this.selectedArea!, this.selectedTopic!)
+  { this.httpCalls.createAreaTopicConnection(this.selectedArea!, this.selectedTopic!.id)
       .subscribe({
         next: (response: void) => {
           this.myPopup.show(SUCCESS_TITLE, SUCCESS_BODY);
--- LPFE/src/app/shared/objects/button.ts
+++ LPFE/src/app/shared/objects/button.ts
@@ -1,5 +1,8 @@
+import { Observable } from "rxjs";
+
 export class Button
 {   name!: string;
     action?: ()=> void
     identifier?: string;
+    disabled?: Observable<boolean>;
 }
